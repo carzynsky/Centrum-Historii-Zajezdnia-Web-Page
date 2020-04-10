2ab9373f5e93f7a7d6179a1f64b9e2fbfe47aa07
Arek <Arek@DESKTOP-U4VRK31>
2020-04-10 15:15:46 +0200

    First measurement consume from Api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
+import {store} from './actions/store';
+import {Provider} from 'react-redux';
 import Home from "./Home";
 import About from "./About";
 import Contact from "./Contact";
 import NavApp from './NavApp';
 import Loggin from './Loggin';
+import Measurement from './Measurement';
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
@@ -11,17 +14,18 @@ class App extends Component
 {
     render(){
         return(
-            <Router>
-                <div>
-                    <NavApp />
-                    <Switch>
-                        <Route path='/Centrum-Historii-Zajezdnia-Web-Page/' exact component={Home}></Route>
-                        <Route path='/Centrum-Historii-Zajezdnia-Web-Page/about' component={About}></Route>
-                        <Route path='/Centrum-Historii-Zajezdnia-Web-Page/contact' component={Contact}></Route>
-                        <Route path='/Centrum-Historii-Zajezdnia-Web-Page/loggin' component={Loggin}></Route>
-                    </Switch>
-                </div>
-            </Router>
+                <Router>
+                    <div>
+                        <NavApp />
+                        <Switch>
+                            <Route path='/Centrum-Historii-Zajezdnia-Web-Page/' exact component={Home}></Route>
+                            <Route path='/Centrum-Historii-Zajezdnia-Web-Page/about' component={About}></Route>
+                            <Route path='/Centrum-Historii-Zajezdnia-Web-Page/contact' component={Contact}></Route>
+                            <Route path='/Centrum-Historii-Zajezdnia-Web-Page/loggin' component={Loggin}></Route>
+                            <Route path='/Centrum-Historii-Zajezdnia-Web-Page/measurement' component={Measurement}></Route>
+                        </Switch>
+                    </div>
+                </Router>
           );
     }
 }
---
+++ src/Measurement.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import {Table} from 'react-bootstrap';
+import './Measurement.css';
+
+class Measurement extends Component {
+    constructor(props){
+        super(props);
+        this.state = {mesm: []};
+    }
+
+    componentDidMount(){
+        this.refreshList();
+    }
+    refreshList(){
+        // this.setState({
+        //     mesm: [{"MeasurementId": 1, "MeasurementTemperature": 23, "MeasurementHumidity": 95, 
+        //     "MeasurementDatetime": "03/04/2020", "MeasurementSensorId": 2}, 
+        //     {"MeasurementId": 2, "MeasurementTemperature": 22, "MeasurementHumidity": 96, 
+        //     "MeasurementDatetime": "03/04/2020", "MeasurementSensorId": 1},
+        //     {"MeasurementId": 3, "MeasurementTemperature": 22, "MeasurementHumidity": 90, 
+        //     "MeasurementDatetime": "01/04/2020", "MeasurementSensorId": 1}]
+        // })
+        fetch('https://localhost:44340/api/measurement')
+        .then(response => response.json())
+        .then(data => {this.setState({mesm:data})});
+
+    }
+  render() {
+    const {mesm} = this.state;
+    console.log(mesm[0]);
+    return (
+        <div className="MeasurementPanel">
+            <Table dark hover borderless>
+              <thead>
+                <tr>
+                  <th>Temperatura</th>
+                  <th>Wilgotność</th>
+                  <th>Data pomiaru</th>
+                  <th>Czujnik</th>
+                </tr>
+              </thead>
+              <tbody>
+                  {mesm.map((mes) =>{
+                    return (
+                      <tr key={mes.id}>
+                      <td>{mes.temperature}</td>
+                      <td>{mes.humidity}</td>
+                      <td>{mes.dateTime}</td>
+                      <td>{mes.sensors.sensorName}</td>
+                      </tr>
+                    )
+                  }
+                    )}
+              </tbody>
+            </Table>
+          </div>
+    );
+  }
+}
+ 
+export default Measurement;
