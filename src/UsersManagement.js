62981a12fec229fa438bbbc496fa3408e97abeb3
Arek <Arek@DESKTOP-U4VRK31>
2020-04-22 11:14:31 +0200

    Add create user in db

--- src/UsersManagement.js
+++ src/UsersManagement.js
@@ -9,6 +9,7 @@ class UsersManagement extends Component {
       // {id: '2', login: 'pracownik1', password: 'pracownik'},
       // {id: '3', login: 'technik1', password: 'technik'}]
       showModal: false,
+      showCreateModal: false,
       responseText: '',
       users: [],
       editUserData: {
@@ -17,18 +18,27 @@ class UsersManagement extends Component {
         password: '',
         userFunctionId: '',
         userFunction: ''
+      },
+      createUserData: {
+        login: '',
+        password: '',
+        userFunctionId: ''
       }
     }
-    this.editUser = this.editUser.bind(this);
+    this.editUserData = this.editUserData.bind(this);
     this.Login = this.Login.bind(this);
     this.Password = this.Password.bind(this);
     this.UserFunctionId = this.UserFunctionId.bind(this);
-    this.putFetch = this.putFetch.bind(this);
-    this.deleteFetch = this.deleteFetch.bind(this);
-    this.getFetch = this.getFetch.bind(this);
+    this.LoginNew = this.LoginNew.bind(this);
+    this.PasswordNew = this.PasswordNew.bind(this);
+    this.UserFunctionIdNew = this.UserFunctionIdNew.bind(this);
+    this.editUser = this.editUser.bind(this);
+    this.deleteUser = this.deleteUser.bind(this);
+    this.getUsers = this.getUsers.bind(this);
+    this.createUser = this.createUser.bind(this);
   }
 
-  async getFetch(){
+  async getUsers(){
     const data = await fetch('https://localhost:44340/api/users');
     const response = await data.json();
 
@@ -38,21 +48,10 @@ class UsersManagement extends Component {
   }
 
   componentDidMount(){
-    this.getFetch();
+    this.getUsers();
   }
 
-Login(event){
-  this.state.editUserData.login = event.target.value;
-}
-
-Password(event){
-  this.state.editUserData.password = event.target.value;
-}
-
-UserFunctionId(event){
-  this.state.editUserData.userFunctionId = event.target.value;
-}
-  editUser(id, login, password, userFunctionId, userFunction){
+  editUserData(id, login, password, userFunctionId, userFunction){
     this.state.editUserData = {
       id: id,
       login: login,
@@ -61,6 +60,42 @@ UserFunctionId(event){
       userFunction: userFunction
     };
     this.handleShow();
+  }
+
+  LoginNew(event){
+    this.state.createUserData.login = event.target.value;
+  }
+
+  PasswordNew(event){
+    this.state.createUserData.password = event.target.value;
+  }
+
+  UserFunctionIdNew(event){
+    this.state.createUserData.userFunctionId = event.target.value;
+  }
+
+  Login(event){
+    this.state.editUserData.login = event.target.value;
+  }
+
+  Password(event){
+    this.state.editUserData.password = event.target.value;
+  }
+
+  UserFunctionId(event){
+    this.state.editUserData.userFunctionId = event.target.value;
+  }
+
+  handleCreateShow = () => {
+    this.setState({
+      showCreateModal: true
+    })
+  }
+
+  handleCreateClose = () => {
+    this.setState({
+      showCreateModal: false
+    })
   }
 
   handleShow = () => {
@@ -75,7 +110,32 @@ UserFunctionId(event){
     });
   }
 
-  deleteFetch(id){
+  async createUser(){
+    console.log(this.state.createUserData);
+    try{
+      fetch('https://localhost:44340/api/users', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+          login: this.state.createUserData.login,
+          password: this.state.createUserData.password,
+          userFunctionId: this.state.createUserData.userFunctionId
+        })
+      })
+      .then(response => {
+        this.getUsers();
+        this.handleCreateClose();
+      })
+    }
+    catch(error){
+      console.log(error);
+    }
+  }
+
+  async deleteUser(id){
     fetch('https://localhost:44340/api/users/'+id, {
       method: 'delete',
       header: {
@@ -84,11 +144,11 @@ UserFunctionId(event){
       }
     })
     .then(response => {
-      this.getFetch();
+      this.getUsers();
     })
   }
-  putFetch()
-  {
+
+  async editUser(){    
     fetch('https://localhost:44340/api/users/'+this.state.editUserData.id, {
       method: 'PUT',
       headers: {
@@ -103,11 +163,11 @@ UserFunctionId(event){
       })
     })
     .then(response => {
-      this.getFetch();
+      this.getUsers();
+      this.handleClose();
     })
-}
+  }
   render() {
-
     const {users} = this.state;
     return (
       <Container className="myContainer">
@@ -115,6 +175,9 @@ UserFunctionId(event){
             <Col>
             <h4>Zarządzanie pracownikami w bazie.</h4>
             </Col>
+        </Row>
+        <Row>
+          <Button className="btnEdit" variant="dark" onClick={this.handleCreateShow}>Dodaj</Button>
         </Row>
         <Row>
           <Table striped bordered hover variant='dark' style={{marginTop: '20px'}}>
@@ -134,8 +197,8 @@ UserFunctionId(event){
                   <td>{u.password}</td>
                   <td>{u.userFunction.function}</td>
                   <td>
-                    <Button className="btnEdit" variant="dark" onClick={this.editUser.bind(this, u.id, u.login, u.password, u.userFunctionId, u.userFunction.function)}>Edytuj</Button>
-                    <Button className="btnDelete" variant="dark" onClick={this.deleteFetch.bind(this, u.id)}>Usuń</Button>
+                    <Button className="btnEdit" variant="dark" onClick={this.editUserData.bind(this, u.id, u.login, u.password, u.userFunctionId, u.userFunction.function)}>Edytuj</Button>
+                    <Button className="btnDelete" variant="dark" onClick={this.deleteUser.bind(this, u.id)}>Usuń</Button>
                   </td>
                 </tr>
                 )
@@ -172,8 +235,38 @@ UserFunctionId(event){
           <Button variant="secondary" onClick={this.handleClose}>
             Zamknij
           </Button>
-          <Button variant="primary" onClick={this.putFetch}>
+          <Button variant="primary" onClick={this.editUser}>
             Zatwierdź
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      <Modal show={this.state.showCreateModal} onHide={this.state.handleCreateClose} className="myModal">
+        <Modal.Header>
+          <Modal.Title>Dodawanie użytkownika</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group>
+              <Form.Label>Login</Form.Label>
+              <Form.Control type="text" defaultValue={this.state.createUserData.login} onChange={this.LoginNew}/>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Hasło</Form.Label>
+              <Form.Control type="text" defaultValue={this.state.createUserData.password} onChange={this.PasswordNew}/>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Funkcja</Form.Label>
+              <Form.Control type="text" defaultValue={this.state.createUserData.userFunctionId} onChange={this.UserFunctionIdNew}/>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={this.handleCreateClose}>
+            Zamknij
+          </Button>
+          <Button variant="primary" onClick={this.createUser}>
+            Dodaj
           </Button>
         </Modal.Footer>
       </Modal>
